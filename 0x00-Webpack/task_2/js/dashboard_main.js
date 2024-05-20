d025e4c3cacaa1c7f7c2ac08224bab224a6fd809
notfoundHeiCho <hajar.znaidi@uit.ac.ma>
2024-05-20 19:55:41 +0100

    Fixing

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -1,8 +1,6 @@
-
-'use strict';
 import '../css/main.css';
-const $ = require('jquery');
-const _ = require('lodash');
+import $ from 'jquery';
+import _ from 'lodash';
 
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
@@ -10,13 +8,11 @@ $('body').append('<button>Click here to get started</button>');
 $('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
-const updateCounter = () => {
-  let clicks = $('#count').html() || 0;
-  $('button').on('click', () => {
-    clicks++;
-    $('#count').html(`${clicks} clicks on the button`);
-  });
+let conunt = 0;
+
+function updateCounter() {
+  count++;
+  $("#count").html(`${count} clicks on the button`);
 };
 
-_.debounce(updateCounter, 500);
-updateCounter();
+$('button').on('click', _.debounce(updateCounter, 500));
