15ba150e2ee7b681385d16a6ac9d1d6561ca3428
notfoundHeiCho <hajar.znaidi@uit.ac.ma>
2024-05-22 17:21:15 +0100

    Create a project using Webpack

---
+++ 0x01-react_intro/task_5/dashboard/dist/bundle.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+    var __webpack_exports__ = {};
+    /*!**********************!*\
+      !*** ./src/index.js ***!
+      \**********************/
+    // src/index.js
+    console.log('Hello, Webpack!');
+    /******/ })()
+    ;
+    //# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUE7QUFDQUEsT0FBTyxDQUFDQyxHQUFHLENBQUMsaUJBQWlCLENBQUMsQyIsInNvdXJjZXMiOlsid2VicGFjazovL2Rhc2hib2FyZC8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyIvLyBzcmMvaW5kZXguanNcbmNvbnNvbGUubG9nKCdIZWxsbywgV2VicGFjayEnKTtcbiJdLCJuYW1lcyI6WyJjb25zb2xlIiwibG9nIl0sInNvdXJjZVJvb3QiOiIifQ==
