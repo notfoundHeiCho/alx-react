c9ace60602d8ef8bcd23a091c4da7867de838276
notfoundHeiCho <hajar.znaidi@uit.ac.ma>
2024-05-22 04:37:38 +0100

    Deploy to a Github page

---
+++ 0x01-react_intro/task_4/dashboard/src/utils.js
@@ -0,0 +1,13 @@
+export const getFullYear = () => {
+    const currentDate = new Date();
+    return currentDate.getFullYear();
+  };
+  
+  export const getFooterCopy = (isIndex) => {
+    return isIndex ? "Holberton School" : "Holberton School main dashboard";
+  };
+
+  export const getLatestNotification = () => {
+    return "<strong>Urgent requirement</strong> - complete by EOD";
+  };
+  
